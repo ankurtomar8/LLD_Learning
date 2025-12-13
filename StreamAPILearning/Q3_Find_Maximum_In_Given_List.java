c5a05cec86de9074eb5d0800928300d3ac115b3b
Dev-Ankur <ankurtomar8@outlook.com>
2025-12-13 19:20:13 +0530

    Add Q3_Find_Maximum_In_Given_List.java to find maximum and minimum elements in a list

---
+++ StreamAPILearning/Q3_Find_Maximum_In_Given_List.java
@@ -0,0 +1,24 @@
+package StreamAPILearning;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q3_Find_Maximum_In_Given_List {
+    public static void main(String[] args){
+
+        List<Integer> list = Arrays.asList(-1,5,4,6,8,7,3,67,52);
+
+        int maxElement = list.stream()
+        .max(Integer::compare)
+        .orElse(null);
+
+        System.out.println("Max element: " + maxElement);
+
+        int minElement = list.stream()
+        .min(Integer::compare)
+        .orElse(null);
+        System.out.println("Min element: " + minElement);
+
+    }
+}
