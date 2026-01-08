f49e5f635e1d23f2bcffc7f4f7656ecb1c893f08
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-08 09:53:47 +0530

    LearningStream

---
+++ JavaFeature_StreamAPI_100/Q_16_Create_a_custom_collectors.java
@@ -0,0 +1,19 @@
+package JavaFeature_StreamAPI_100;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Q_16_Create_a_custom_collectors {
+    public static void main(String[] s){
+
+           List<String> list = Arrays.asList("Artificial", "Intelligence", 
+        "Engineering", "Learning");
+
+        String joined = list.stream().collect(Collectors.joining());
+
+        System.out.println("String joined using : \n"+joined);
+        
+    }
+    
+}
