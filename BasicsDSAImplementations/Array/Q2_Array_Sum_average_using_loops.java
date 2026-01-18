dc439815e4012f0fb228d85662d121103800e539
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-18 23:44:43 +0530

    Array Sum

---
+++ BasicsDSAImplementations/Array/Q2_Array_Sum_average_using_loops.java
@@ -0,0 +1,22 @@
+package BasicsDSAImplementations.Array;
+
+public class Q2_Array_Sum_average_using_loops {
+    public static void main(String[] args){
+        int[] arraySum = {1,2,3,4,5,6,7,8,9};
+
+        int sum = 0;
+        for(int sumCount : arraySum){
+            sum += sumCount;
+        }
+        System.out.println(" "+sum);
+
+         double b = arraySum.length;
+         System.out.println("Sum is \n"+sum);
+         double newAverage=(double)(sum/b);
+
+         System.out.println("Average is "+newAverage);
+
+
+
+    }
+}
