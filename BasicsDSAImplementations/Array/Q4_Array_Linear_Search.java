5b5d0260f88a87e87d64c54a6d2ca54e88a80bac
Dev-Ankur <ankurtomar8@outlook.com>
2026-01-18 23:56:16 +0530

    LinearSearch

---
+++ BasicsDSAImplementations/Array/Q4_Array_Linear_Search.java
@@ -0,0 +1,16 @@
+package BasicsDSAImplementations.Array;
+
+public class Q4_Array_Linear_Search {
+    public static void main(String[] args){
+        int[] array = {10,20,30,50,40,90,34,65,96,75,85};
+
+        int key = 75;
+        for(int i=0; i < array.length; i++){
+            if(array[i] == key){
+                System.out.print("Key Found at index "+i);
+                break; // Key Found at index 9
+            }
+        }
+        
+    }
+}
